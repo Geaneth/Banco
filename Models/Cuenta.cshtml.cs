6b669b6281eac0eda08f92f594ab3d4ed6955150
lourdes <lourdesgeaneth@gmail.com>
2024-04-27 11:00:10 -0500

    Conexion DB

--- Models/Cuenta.cshtml.cs
+++ Models/Cuenta.cshtml.cs
@@ -11,6 +11,7 @@ namespace Banco.Models
         public int Id {   get; set;}
         public string? NombreTitular { get; set; }
         public string? TipoCuenta { get; set; }
+        
         public decimal SaldoInicial { get; set; }
         public string? Email { get; set; }
     }
